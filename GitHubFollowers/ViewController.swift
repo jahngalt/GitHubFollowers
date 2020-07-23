08500cf8b4b5f349bce890e6029389135057d631
Oleg Kudimov <ovkudimov@gmail.com>
2020-07-23 19:48:34 +0300

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Oleg Kudimov on 7/23/20.
+//  Copyright © 2020 Oleg Kudimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
